0940056e749d54a269ade058548d593552e941bb
Kevin Brammer <kevin.brammer@nanotempertech.com>
2022-05-25 14:43:30 +0200

    Rework stuff to be better. Not tested, serial data and timestamp still missing but infrastructure was created

--- desktop_app/main.py
+++ desktop_app/main.py
@@ -1,69 +1,61 @@
-import os
 import struct
-import time
 import asyncio
-import pandas as pd
 from bleak import BleakClient, discover
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 import multiprocessing as mp
-from queue import Queue, Empty
+from queue import Empty
 import numpy as np
 from collections import deque
+from typing import Deque
 
-ACCEL_STRING_UUID = 'fc0a2501-af4b-4c14-b795-a49e9f7e6b84'
+BLE_DATA_UUID = 'fc0a2501-af4b-4c14-b795-a49e9f7e6b84'
 
 MAXIMUM_RUNTIME = 600  # in s
-
 PLOT_UPDATE_INTERVAL = 100  # in ms
+NUMBER_OF_DATA_POINTS = 5000
 
-NUMBER_OF_DATAPOINTS = 5000
 
-CSV_FILE_PATH = "out.csv"
+class DataPoint(object):
+    def __init__(self, timestamp: float, gyro_x: float, gyro_y: float, gyro_z: float, accel_x: float, accel_y: float, accel_z: float):
+        self.timestamp: float = timestamp
 
-timestamp_name = "timestamp"
+        self.gyro_x: float = gyro_x
+        self.gyro_y: float = gyro_y
+        self.gyro_z: float = gyro_z
 
-gyro_x_name = "gyro_x"
-gyro_y_name = "gyro_y"
-gyro_z_name = "gyro_z"
-
-accel_x_name = "accel_x"
-accel_y_name = "accel_y"
-accel_z_name = "accel_z"
+        self.accel_x: float = accel_x
+        self.accel_y: float = accel_y
+        self.accel_z: float = accel_z
 
 
 class Nano33BLE(object):
-    def __init__(self, data_queue):
-        # self.df = namespace.df
+    def __init__(self, plot_q: mp.Queue, serial_q: mp.Queue):
+        self.plot_queue: mp.Queue = plot_q
+        self.serial_queue: mp.Queue = serial_q
 
-        self.data_queue = data_queue
-
-        if os.path.exists(CSV_FILE_PATH):
-            os.remove(CSV_FILE_PATH)
+        self.current_datapoint = 0  # Temporary until we get timestamp
 
     def callback_data(self, _, data):
-        floats = list()
+        floats = deque()
         while len(data) > 0:
             floats.append(struct.unpack("f", data[0:4])[0])
             del data[0:4]
 
-        new_df = pd.DataFrame()
+        for i in range(0, len(floats), 6):
+            dp = DataPoint(self.current_datapoint,  # Placeholder. we want to add the actual timestamp in seconds here, but it is not yet sent by the arduino
+                           floats[i + 0],
+                           floats[i + 1],
+                           floats[i + 2],
+                           floats[i + 3],
+                           floats[i + 4],
+                           floats[i + 5])
 
-        # We always send groups of 6 values, so we pick every sixth value to add to the specific list
-        new_df[gyro_x_name] = floats[0::6]
-        new_df[gyro_y_name] = floats[1::6]
-        new_df[gyro_z_name] = floats[2::6]
+            self.current_datapoint += 1
 
-        new_df[accel_x_name] = floats[3::6]
-        new_df[accel_y_name] = floats[4::6]
-        new_df[accel_z_name] = floats[5::6]
-
-        for fl in new_df[accel_x_name]:
-            self.data_queue.put(fl)
-
-        # self.df = pd.concat([self.df, new_df], ignore_index=True)
-
-        # self.df.to_csv(CSV_FILE_PATH)
+            # We send the data once to the plotter and once to the serial writer
+            self.plot_queue.put(dp)
+            self.serial_queue.put(dp)
 
     async def gather_data(self):
         print('Arduino Nano BLE Peripheral Central Service')
@@ -77,68 +69,126 @@ class Nano33BLE(object):
                     async with BleakClient(d.address) as client:
                         print(f'Connected to {d.address}')
 
-                        await client.start_notify(ACCEL_STRING_UUID, self.callback_data)
+                        await client.start_notify(BLE_DATA_UUID, self.callback_data)
 
                         await asyncio.sleep(MAXIMUM_RUNTIME)
 
-                        await client.stop_notify(ACCEL_STRING_UUID)
+                        await client.stop_notify(BLE_DATA_UUID)
 
     def process(self):
         asyncio.run(self.gather_data())
 
 
-def init_plot():
-    pass
+class Plotter(object):
+    def __init__(self, dq: mp.Queue):
+        self.data_queue = dq
+        self.animation = None
 
+        # Plot init
+        self.figure, self.axes = plt.subplots(2)
 
-def animate(frame, data_queue, x_accel_line):
-    try:
-        y = x_accel_line.get_ydata()
+        self.accel_plots = self.axes[0, 0]
+        self.gyro_plots = self.axes[0, 1]
 
-        new_data = deque()
+        self.accel_plots.set_title("Accelerations")
+        self.accel_plots.set_xlabel("Timestamp [s]")
+        self.accel_plots.set_ylabel("Acceleration [G]")
+        self.accel_plots.set_xlim(0, NUMBER_OF_DATA_POINTS)
+        self.accel_plots.set_ylim(-2, 2)
+
+        self.gyro_plots.set_title("Gyro")
+        self.gyro_plots.set_xlabel("Timestamp [s]")
+        self.gyro_plots.set_ylabel("Position")
+        self.gyro_plots.set_xlim(0, NUMBER_OF_DATA_POINTS)
+        self.gyro_plots.set_ylim(-100, 100)
+
+        plt.legend()
+
+        self.accel_x_plot = self.accel_plots.plot([], [], label="Accel X")
+        self.accel_y_plot = self.accel_plots.plot([], [], label="Accel Y")
+        self.accel_z_plot = self.accel_plots.plot([], [], label="Accel Z")
+
+        self.gyro_x_plot = self.gyro_plots.plot([], [], label="Gyro X")
+        self.gyro_y_plot = self.gyro_plots.plot([], [], label="Gyro Y")
+        self.gyro_z_plot = self.gyro_plots.plot([], [], label="Gyro Z")
+
+    def animate(self, frame: int):
+        accel_x_y = self.accel_x_plot.get_ydata()
+        accel_y_y = self.accel_y_plot.get_ydata()
+        accel_z_y = self.accel_z_plot.get_ydata()
+
+        gyro_x_y = self.gyro_x_plot.get_ydata()
+        gyro_y_y = self.gyro_y_plot.get_ydata()
+        gyro_z_y = self.gyro_z_plot.get_ydata()
+
+        x = self.accel_x_plot.get_xdata()
+
+        new_data: Deque[DataPoint] = deque()
 
         while True:
             try:
-                # Get the index we sent in
-                new_data.append(data_queue.get(False))
+                new_data.append(plot_queue.get(False))
 
             except Empty:
                 break
 
-        y = np.append(y, new_data)[-NUMBER_OF_DATAPOINTS:]
+        for dp in new_data:
+            dp: DataPoint
 
-        x = np.array(list(range(0, len(y))))
-        x_accel_line.set_data(x, y)
-        return x_accel_line,
-    except AttributeError as e:
+            x = np.append(x, dp.timestamp)
+
+            accel_x_y = np.append(accel_x_y, dp.accel_x)[-NUMBER_OF_DATA_POINTS:]
+            accel_y_y = np.append(accel_y_y, dp.accel_y)[-NUMBER_OF_DATA_POINTS:]
+            accel_z_y = np.append(accel_z_y, dp.accel_z)[-NUMBER_OF_DATA_POINTS:]
+
+            gyro_x_y = np.append(gyro_x_y, dp.gyro_x)[-NUMBER_OF_DATA_POINTS:]
+            gyro_y_y = np.append(gyro_y_y, dp.gyro_y)[-NUMBER_OF_DATA_POINTS:]
+            gyro_z_y = np.append(gyro_z_y, dp.gyro_z)[-NUMBER_OF_DATA_POINTS:]
+
+        self.accel_x_plot.set_data(x, accel_x_y)
+        self.accel_y_plot.set_data(x, accel_y_y)
+        self.accel_z_plot.set_data(x, accel_z_y)
+
+        self.gyro_x_plot.set_data(x, gyro_x_y)
+        self.gyro_y_plot.set_data(x, gyro_y_y)
+        self.gyro_z_plot.set_data(x, gyro_z_y)
+
+        return self.accel_x_plot, self.accel_y_plot, self.accel_z_plot, self.gyro_x_plot, self.gyro_y_plot, self.gyro_z_plot
+
+    def plot(self):
+        self.animation = FuncAnimation(self.figure, self.animate, blit=True, interval=PLOT_UPDATE_INTERVAL)
+
+        plt.show()
+
+
+class SerialSender(object):
+    def __init__(self, serial_q: mp.Queue):
+        self.data_queue = serial_q
+
+    def process(self):
         pass
-
-
-def plot(data_queue):
-    figure = plt.figure()
-    axis = plt.axes(xlim=(0, NUMBER_OF_DATAPOINTS),
-                    ylim=(-2, 2))
-    line, = axis.plot([], [])
-
-    anim = FuncAnimation(figure, animate, blit=True, interval=PLOT_UPDATE_INTERVAL, fargs=(data_queue, line))
-
-    plt.show()
 
 
 if __name__ == '__main__':
     try:
-        data_queue = mp.Queue()
+        plot_queue = mp.Queue()
+        serial_queue = mp.Queue()
 
-        ard = Nano33BLE(data_queue)
+        ard = Nano33BLE(plot_queue, serial_queue)
+        plotter = Plotter(plot_queue)
+        serial = SerialSender(serial_queue)
 
         data_p = mp.Process(target=ard.process)
-        plot_p = mp.Process(target=plot, args=(data_queue, ))
+        plot_p = mp.Process(target=plotter.plot)
+        serial_p = mp.Process(target=serial.process)
 
         data_p.start()
         plot_p.start()
+        serial_p.start()
 
         data_p.join()
         plot_p.join()
+        serial_p.join()
     except KeyboardInterrupt:
         print('\nReceived Keyboard Interrupt')
     finally:
